cdece14a9cb7e366b4d668739fb3005063e70e2f
Son <son@tapuniverse.com>
2025-07-04 10:22:46 +0700

    init package

---
+++ lib/ffmpeg_kit_flutter_full_gpl_package.dart
@@ -0,0 +1,37 @@
+library ffmpeg_kit_flutter_full_gpl_package;
+export 'arch_detect.dart';
+export './media_information_json_parser.dart';
+export './packages.dart';
+
+export './src/common/enum/log_redirection_strategy.dart';
+export './src/common/enum/session_state.dart';
+export './src/common/enum/signal.dart';
+
+export './src/common/type/ffmpeg_session_complete_callback.dart';
+export './src/common/type/ffprobe_session_complete_callback.dart';
+export './src/common/type/log_callback.dart';
+export './src/common/type/media_information_session_complete_callback.dart';
+export './src/common/type/statistics_callback.dart';
+
+export './src/config/ffmpeg_kit_config.dart';
+
+export './src/factory/ffmpeg_kit_factory.dart';
+
+export './src/ffmpeg_kit_flutter_initializer.dart';
+
+export './src/kit/ffmpeg_kit.dart';
+export './src/kit/ffprobe_kit.dart';
+
+export './src/model/chapter.dart';
+export './src/model/level.dart';
+export './src/model/log.dart';
+export './src/model/media_information.dart';
+export './src/model/return_code.dart';
+export './src/model/session.dart';
+export './src/model/statistics.dart';
+export './src/model/stream_information.dart';
+
+export './src/session/abstract_session.dart';
+export './src/session/ffmpeg_session.dart';
+export './src/session/ffprobe_session.dart';
+export './src/session/media_information_session.dart';
